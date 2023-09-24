ac98a62823c1caf90919ad52ef6fdf6f2b8ee529
Benjamin Hall <hallbkb@gmail.com>
2023-09-24 08:25:18 -0600

    Added two buttons

--- meal.py
+++ meal.py
@@ -1,21 +1,7 @@
 from tkinter import *
 
-root = Tk()
-
-# Title widget for the program
-title = Label(root, text="Welcome to the Meal Decider!\nAdd your food to the food list and we'll tell you what you can make!")
-title.pack()
-
-root.mainloop()
-
-'''
-print('*'*20)
-print("Welcome to the Meal Decider!\nAdd your food to the food list and we'll tell you what you can make!")
-print('*'*20)
-'''
-
 # Readlines for list of food
-food = open("Food-List", 'r')
+food = open("foodList", 'r')
 x = food.readlines()
 food.close
 
@@ -49,18 +35,57 @@ def organizeLst(header,section):
                 lst.append(foodLst[n])
     return lst
 
-for i in range(len(recipesLst)-1):
-    if recipesLst[i] == '-':
-        ingredients = []
-        x = i + 1
-        while recipesLst[x] != '-':
-            ingredients.append(recipesLst[x+1])
-            x += 1
-        if all(elem in foodLst for elem in ingredients):
-            print('-'*20)
-            print("You have the ingredients to cook " + recipesLst[i+1].strip(":") + "!")
-            ingredients.pop()
-            print("Ingredients Requred: ", ingredients)
-            print('-'*20)
+
+
+def displayFood():
+    for i in range(len(foodLst)-1):
+        if foodLst[i] == '-':
+            items = []
+            x = i + 1
+            while foodLst[x+1] != '-':
+                items.append(str(foodLst[x+1]) + ',')
+                x += 1
+            area = Label(root, text=foodLst[i+1])
+            foodLabel = Label(root, text=items)
+            area.pack()
+            foodLabel.pack()
+
+def displayMeals():
+    for i in range(len(recipesLst)-1):
+        if recipesLst[i] == '-':
+            ingredients = []
+            x = i + 1
+            while recipesLst[x] != '-':
+                ingredients.append(recipesLst[x+1])
+                x += 1
+            if all(elem in foodLst for elem in ingredients):
+                mtext = "You have the ingredients to cook " + recipesLst[i+1].strip(":") + "!"
+                m = Label(root, text=mtext)
+                m.pack()
+                ingredients.pop()
+                rtext = "Ingredients Requred: ", ingredients
+                r = Label(root, text=rtext)
+                r.pack()
+
+
+root = Tk()
+root.geometry("600x450")
+# Title widget for the program
+title = Label(root, text="Welcome to the Meal Detector!")
+desc = Label(root, text="Start by selecting any of the options below.")
+# Buttons
+foodBut = Button(root, text="Display Food", command=displayFood)
+recipeBut = Button(root, text="Display Recipes",)
+mealBut = Button(root, text="Display Meals!", command=displayMeals)
+
+# Layout commands
+title.pack()
+desc.pack()
+foodBut.pack()
+recipeBut.pack()
+mealBut.pack()
+
+root.mainloop()
+
 
 
